889f953c62920bdafb5ea99ab6b9f2086ab97016
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-03 23:08:25 -0300

    Agregue el componente noticia

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,7 +1,12 @@
 import { Container } from "react-bootstrap";
+import Noticia from "./Noticia";
 
 const ListaNoticias = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Noticia />
+    </Container>
+  );
 };
 
 export default ListaNoticias;
