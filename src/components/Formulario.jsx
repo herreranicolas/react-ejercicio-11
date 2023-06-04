1a206db158b7832d819adffced0b23e38030f855
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-03 23:54:24 -0300

    Agregue las categorias para realizar la consulta a la api , tambien agregue use effect de prueba que toma el estado de cuando hay un cambio de categoria y indica en un console log que debe consultar la api en un futuro

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,14 @@
+import { useState, useEffect } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 import ListaNoticias from "./ListaNoticias";
 
 function Formulario() {
+  const [categoria, setCategoria] = useState("");
+
+  useEffect(() => {
+    console.log("Consultar API");
+  }, [categoria]);
+
   return (
     <>
       <Form className="p-3 border border-2 border-dark border-bottom-0">
@@ -12,17 +19,34 @@ function Formulario() {
           <Form.Label
             column
             sm={2}
+            md={3}
             htmlFor="categoria"
-            className="p-0 text-end"
+            className="p-0 text-center text-lg-end mb-2 mb-md-0"
           >
             Buscar por categoría:
           </Form.Label>
           <Col sm={10} md={4}>
-            <Form.Select name="categoria" id="categoria">
-              <option>Seleccionar categoria</option>
-              <option value="1">Categoria 1</option>
-              <option value="2">Categoria 2</option>
-              <option value="3">Categoria 3</option>
+            <Form.Select
+              name={categoria}
+              id="categoria"
+              onChange={(e) => {
+                setCategoria(e.target.value);
+              }}
+              value={categoria}
+            >
+              <option value="">Seleccionar categoria</option>
+              <option value="business">Negocios</option>
+              <option value="entertainment">Entretenimiento</option>
+              <option value="environment">Medio Ambiente</option>
+              <option value="food">Comida</option>
+              <option value="health">Salud</option>
+              <option value="politics">Política</option>
+              <option value="science">Ciencia</option>
+              <option value="sports">Deportes</option>
+              <option value="technology">Tecnología</option>
+              <option value="top">Top noticias</option>
+              <option value="tourism">Turismo</option>
+              <option value="world">Mundo</option>
             </Form.Select>
           </Col>
         </Form.Group>
