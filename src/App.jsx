9fe8e9ab08681348fb28aa7a4f1e895bc3ee99c6
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-03 22:53:14 -0300

    Agregue el componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Titulo from "./components/Titulo";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <>
       <Titulo />
+      <Formulario />
     </>
   );
 }
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,19 @@
+import { Form } from "react-bootstrap";
+
+function Formulario() {
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label htmlFor="categoria">Buscar por categoría:</Form.Label>
+        <Form.Select name="categoria" id="categoria">
+          <option>Seleccionar categoria</option>
+          <option value="1">Categoria 1</option>
+          <option value="2">Categoria 2</option>
+          <option value="3">Categoria 3</option>
+        </Form.Select>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default Formulario;
